cdf3dd2fb054499ee876c3678f57c643502bbe56
thesimplezero <hi@thesimplezero.io>
2024-01-03 22:30:00 +0300

    upload task 8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,11 @@
+// Performs division, ensuring denominator is not zero
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      throw new Error();
+    }
+    return numerator / denominator;
+  } catch (error) {
+    throw Error('cannot divide by 0');
+  }
+}
