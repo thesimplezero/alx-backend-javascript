77a55da06f47824bccc70c3c826f4453a3e87d27
thesimplezero <hi@thesimplezero.io>
2024-01-04 22:19:31 +0300

    "Add ClassRoom class with constructor setting maximum student capacity."

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+// Class representing a classroom.
+export default class ClassRoom {
+  /**
+   * Constructor for the ClassRoom class.
+   * @param {number} maxStudentsSize - The maximum number of students allowed in the classroom.
+   */
+  constructor(maxStudentsSize) {
+    // Setting the maximum number of students for the classroom.
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
