51a0abce05fabc77df4a0b21a146df796fd24281
thesimplezero <hi@thesimplezero.io>
2024-01-10 00:57:12 +0300

    update code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,3 @@
-// ES6 default export of a function that retrieves a list of student objects with specified attributes.
-// Each object includes an id, firstName, and location, representing student information.
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
