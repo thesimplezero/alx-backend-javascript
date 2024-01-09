d7cd237854c104eb7750434cc80adcba640ef1b3
thesimplezero <hi@thesimplezero.io>
2024-01-10 00:49:59 +0300

    Exported setFromArray function for creating Set with unique values

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+// Creates and returns a Set from the elements of the provided array.
+// Preserves only unique values, eliminating any duplicate entries in the resulting Set.
+export default function setFromArray(arr) {
+  return new Set(arr);
+}
